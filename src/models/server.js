b09b3eca963939cf0a0613163311cc2a11b11fcf
Fecso <Fecsoul@gmail.com>
2022-09-25 22:34:28 -0300

    react-script downgrade

--- src/models/server.js
+++ src/models/server.js
@@ -1,18 +1,19 @@
 //Servidor de Express
 const express  = require('express');
-const http     = require('http');
+const https     = require('https');
 const socketIO = require('socket.io');
 const path     = require('path');
 const Sockets  = require('./sockets');
 const cors     = require('cors');
+
 class Server {
 
     constructor(){
         this.app = express();
-        this.port =8082;
+        this.port =8080;
 
         //Http Server
-        this.server = http.createServer(this.app);
+        this.server = https.createServer(this.app);
       
         //Configuracion del socket server
         this.io = socketIO(this.server, {/* configuraciones */});
